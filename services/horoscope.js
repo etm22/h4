2f4884b1c39f5e0b930a80940396a83041397f6a
etm22 <easytubemastery@gmail.com>
2023-12-03 18:32:31 +0400

    fix scraper

--- services/horoscope.js
+++ services/horoscope.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const moment = require("moment");
 const { parse } = require("node-html-parser");
 
-async function scrapeHoroscope(sign) {
+async function scrapeHoroscopeOld(sign) {
   const response = await axios.get(
     `https://www.huffpost.com/horoscopes/${sign}`
   );
@@ -18,6 +18,28 @@ async function scrapeHoroscope(sign) {
     sign: data.sign,
     horoscope: data.content,
   };
+}
+
+async function scrapeHoroscope(sign) {
+  const date = moment().format("MMMM-D-YYYY").toLocaleLowerCase();
+
+  const response = await axios.get(
+    `https://www.vogue.in/horoscope/collection/horoscope-today-${date}/`
+  );
+  const root = parse(response.data);
+  const blocks = root.querySelectorAll(".product-summary");
+
+  const horoscopes = blocks.slice(0, 12).map((b) => {
+    return {
+      date: moment().format("DD MMMM YYYY"),
+      sign: b.parentNode.previousElementSibling.previousElementSibling.textContent
+        .split(" ")[0]
+        .toLowerCase(),
+      horoscope: b.textContent.split("\n")[0],
+    };
+  });
+
+  return horoscopes.filter((h) => h.sign == sign)[0];
 }
 
 module.exports = { scrapeHoroscope };
